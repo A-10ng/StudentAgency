c5612aff444909186d9a95666ecc35fe8f3d0ac2
LongSh1z <2674461089@qq.com>
2019-10-20 11:11:11 +0800

    Improve login UI!

--- app/src/main/java/com/example/studentagency/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/studentagency/ui/activity/LoginActivity.java
@@ -11,11 +11,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.studentagency.R;
 import com.example.studentagency.bean.Cat;
 import com.example.studentagency.mvp.presenter.LoginBasePresenter;
 import com.example.studentagency.mvp.view.LoginBaseView;
+
+import net.frakbot.jumpingbeans.JumpingBeans;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
@@ -31,6 +34,7 @@ public class LoginActivity extends AppCompatActivity implements LoginBaseView {
     private String studentId = "";   //用户输入的学号
     private String password = "";    //用户输入的密码
     private MyHandler mHandler = new MyHandler(this);
+    private TextView tv_description,tv_studentIdLength,tv_passwordLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +53,9 @@ public class LoginActivity extends AppCompatActivity implements LoginBaseView {
         //初始化控件
         initViews();
 
+        //设置文字跳动
+        setLettersJump();
+
         //开启监听EditText
         mHandler.sendEmptyMessage(LISTEN_EDITTEXT);
 
@@ -56,10 +63,23 @@ public class LoginActivity extends AppCompatActivity implements LoginBaseView {
         setViewsOnClick();
     }
 
+    private void setLettersJump() {
+        JumpingBeans jumpingBeans = JumpingBeans
+                .with(tv_description)
+                .makeTextJump(3,7)
+                .setIsWave(true)
+                .setLoopDuration(1200)
+                //.setAnimatedDutyCycle(1.0f)
+                .build();
+    }
+
     private void initViews() {
         btn_login = findViewById(R.id.btn_login);
         et_studentId = findViewById(R.id.et_studentId);
         et_password = findViewById(R.id.et_password);
+        tv_description = findViewById(R.id.tv_description);
+        tv_studentIdLength = findViewById(R.id.tv_studentIdLength);
+        tv_passwordLength = findViewById(R.id.tv_passwordLength);
     }
 
     private void setViewsOnClick() {
@@ -119,12 +139,44 @@ public class LoginActivity extends AppCompatActivity implements LoginBaseView {
                         //获取输入框的内容
                         studentId = et_studentId.getText().toString();
                         password = et_password.getText().toString();
+                        int studentIdLength = studentId.length();
+                        int passwordLength = password.length();
                         //如果两者的长度符合条件了，登录按钮就处于可按状态，否则就不可按
-                        if (studentId.length() == STUDENTID_LENGTH && password.length() == PASSWORD_LENGTH) {
+                        if (studentIdLength == STUDENTID_LENGTH && passwordLength == PASSWORD_LENGTH) {
                             btn_login.setEnabled(true);
                         } else {
                             btn_login.setEnabled(false);
                         }
+
+                        /**
+                         * 监听输入框下方的文字长度显示
+                         */
+                        //监听学号下方的文字
+                        if (studentIdLength > 0 && studentIdLength < STUDENTID_LENGTH){
+                            tv_studentIdLength.setVisibility(View.VISIBLE);
+                            tv_studentIdLength.setText(studentIdLength+"/"+STUDENTID_LENGTH);
+                            tv_studentIdLength.setTextColor(getResources().getColor(R.color.red));
+                        }
+                        else if (studentIdLength == STUDENTID_LENGTH) {
+                            tv_studentIdLength.setText(studentIdLength+"/"+STUDENTID_LENGTH);
+                            tv_studentIdLength.setTextColor(getResources().getColor(R.color.green));
+                        }
+                        else{
+                            tv_studentIdLength.setVisibility(View.GONE);
+                        }
+
+                        //监听密码下方的文字
+                        if (passwordLength > 0 && passwordLength < PASSWORD_LENGTH){
+                            tv_passwordLength.setVisibility(View.VISIBLE);
+                            tv_passwordLength.setText(passwordLength+"/"+PASSWORD_LENGTH);
+                            tv_passwordLength.setTextColor(getResources().getColor(R.color.red));
+                        }else if (passwordLength == PASSWORD_LENGTH) {
+                            tv_passwordLength.setText(passwordLength+"/"+PASSWORD_LENGTH);
+                            tv_passwordLength.setTextColor(getResources().getColor(R.color.green));
+                        }else {
+                            tv_passwordLength.setVisibility(View.GONE);
+                        }
+
                         mHandler.sendEmptyMessageDelayed(LISTEN_EDITTEXT, 100);
                         break;
                 }
